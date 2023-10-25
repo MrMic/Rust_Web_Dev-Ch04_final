0ffec33d5ea2d958c843b5fda445ec00352a75a9
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2023-10-25 14:42:47 +0200

    Log4rs + Warp Log Filter

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,26 @@ mod types;
 /// WRAP Server
 #[tokio::main]
 async fn main() {
+    // env_logger::init();
+    log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
+
+    //      ━━━━━━━━━━━━━━━━━━━━━━━ LOGGING With WRAP ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+    let log = warp::log::custom(|info| {
+        eprintln!(
+            "{} {} {} {:?} from {} with {:?}",
+            info.method(),
+            info.path(),
+            info.status(),
+            info.elapsed(),
+            info.remote_addr().unwrap(),
+            info.request_headers(),
+        );
+    });
+
+    log::error!("This is an error!");
+    log::info!("This is an info!");
+    log::warn!("This is a warning!");
+
     let store = store::Store::new();
     let store_filter = warp::any().map(move || store.clone());
 
@@ -66,6 +86,7 @@ async fn main() {
         .or(delete_question)
         .or(add_answer)
         .with(cors)
+        .with(log)
         .recover(return_error);
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
